784b8e6d3b7f0c213e24c4da5fa57aa87d68608d
eprikhodko <eprikhodkodev@gmail.com>
2022-04-10 22:15:40 +0300

    feat: clean up slider code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import './scss/main.scss';
 
 console.log('hello from webpack!');
 
-const slider = document.querySelector('.slider__content');
+const slider = document.querySelector('.slider');
 const slides = document.querySelectorAll('.slider__slide');
 const sliderTabs = document.querySelectorAll('.slider__tab-item');
+const activeSlide = document.getElementsByClassName('slider__slide_active');
+const activeTab = document.getElementsByClassName('slider__tab-item_active');
 const prevButton = document.querySelector('.slider__button-prev');
 const nextButton = document.querySelector('.slider__button-next');
 
 let slideNumber = 3;
 
+// этот блок отвечает за кнопки под слайдером, за ручное переключение слайдов по клику мыши
 const manualNav = (index) => {
   slides.forEach((slide) => {
     slide.classList.remove('slider__slide_active');
@@ -29,6 +32,8 @@ sliderTabs.forEach((tab, i) => {
   });
 });
 
+// этот блок отвечает за кнопки переключения картинок на маленьких экранах,
+// они появляются после того как табы под слайдером исчезают
 nextButton.addEventListener('click', () => {
   slides.forEach((slide) => {
     slide.classList.remove('slider__slide_active');
@@ -57,11 +62,9 @@ prevButton.addEventListener('click', () => {
   slides[slideNumber].classList.add('slider__slide_active');
 });
 
-const activeSlide = document.getElementsByClassName('slider__slide_active');
-const activeTab = document.getElementsByClassName('slider__tab-item_active');
-
 let play;
 
+// этот блок отвечает за автоматическое повторение слайдов по кругу
 const repeater = () => {
   let i = 0;
 
@@ -84,14 +87,16 @@ const repeater = () => {
     if (i >= slides.length) {
       return;
     }
-  }, 1000);
+  }, 3000);
 };
 repeater();
 
+// если мы навели мышку на слайдер, автоматическая смена картинок будет приостановлена
 slider.addEventListener('mouseover', () => {
   clearInterval(play);
 });
 
+// если мы убрали курсор мыши со слайдера, автоматическая смена картинок будет возобновлена
 slider.addEventListener('mouseout', () => {
   repeater();
 });
