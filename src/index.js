06b36c5b7e7e4f7a29b6b18adb0fbe57c9757bb9
eprikhodko <eprikhodkodev@gmail.com>
2022-04-10 12:54:46 +0300

    feat: add 'active' class to buttons and slides on click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './scss/main.scss';
 
 console.log('hello from webpack!');
+
+const slides = document.querySelectorAll('.slider__slide');
+const btns = document.querySelectorAll('.slider__tab-item');
+
+let currentSlide = 1;
+
+const manualNav = (manual) => {
+  slides[manual].classList.add('active');
+  btns[manual].classList.add('active');
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
